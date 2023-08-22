a3cae8b0e34f2f95582240eaec80537b494fe522
iki-rumondor <ikirumondor07@gmail.com>
2023-08-23 03:56:03 +0800

    feat(main): using say hello module

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/iki-rumondor/app_say_hello
+
+go 1.20
+
+require github.com/iki-rumondor/say_hello v1.0.0
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import(
+	say_hello "github.com/iki-rumondor/say_hello"
+)
+
+
+func main(){
+	say_hello.SayHello()
+}
